16fd44467603253b3ff09750ae7cbfbce8ec2341
Matheus Bereta <matheus_bereta@hotmail.com>
2022-05-18 11:45:20 -0300

    :lipstick: Adding choice_image widget

---
+++ lib/app/core/design_system/widgets/choice_image.widget.dart
@@ -0,0 +1,43 @@
+import 'package:app_pelada_plus/app/core/design_system/tokens/paddings.token.dart';
+import 'package:flutter/material.dart';
+
+class ChoiceImageWidget extends StatelessWidget {
+  const ChoiceImageWidget({
+    Key? key,
+    required this.image,
+    required this.isSelected,
+    required this.action,
+  }) : super(key: key);
+
+  final String image;
+  final bool isSelected;
+  final VoidCallback action;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: Padding(
+        padding: EdgeInsets.only(bottom: TokenPaddings.lg),
+        child: Container(
+          decoration: isSelected
+              ? BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.green, width: 3),
+                )
+              : null,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(
+              'assets/images/$image.png',
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
