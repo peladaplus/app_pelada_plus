c84d6db38c4c9243e7994df84e4e38d3c12a3ee0
Matheus Bereta <matheus_bereta@hotmail.com>
2022-05-18 22:56:31 -0300

    :lipstick: Adding animation widget

--- lib/app/core/design_system/widgets/animation.widget.dart
+++ lib/app/core/design_system/widgets/animation.widget.dart
@@ -8,6 +8,7 @@ class AnimationWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Lottie.asset(
       'assets/animations/success.json',
+      height: MediaQuery.of(context).size.height / 2.5,
       repeat: false,
     );
   }
