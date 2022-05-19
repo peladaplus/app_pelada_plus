4e16170a0f3390a01d5d580e2cd154ec9a3edd62
Matheus Bereta <matheus_bereta@hotmail.com>
2022-05-19 12:17:54 -0300

    :lipstick: Adding font family in ThemeData

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Pelada+',
+      theme: ThemeData(fontFamily: GoogleFonts.sourceSansPro().fontFamily),
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
